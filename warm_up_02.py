d68c88159181d4910e1a53fd11f96d685cabd715
Redhead <86776617+alin2025@users.noreply.github.com>
2022-06-12 16:23:10 +0300

    Create warm_up_02.py

---
+++ warm_up_02.py
@@ -0,0 +1,25 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+
+#   Initialize the Spark session
+spark = SparkSession.builder \
+    .master("local") \
+    .config("spark.sql.autoBroadcastJoinThreshold", -1) \
+    .config("spark.executor.memory", "500mb") \
+    .appName("Exercise1") \
+    .getOrCreate()
+
+
+#   Read the source tables in Parquet format
+products_table = spark.read.parquet("./products_parquet")
+sales_table = spark.read.parquet("./sales_parquet")
+sellers_table = spark.read.parquet("./sellers_parquet")
+
+#   Output how many products have been actually sold at least once
+print("Number of products sold at least once")
+sales_table.agg(countDistinct(col("product_id"))).show()
+
+#   Output which is the product that has been sold in more orders
+print("Product present in more orders")
+sales_table.groupBy(col("product_id")).agg(
+    count("*").alias("cnt")).orderBy(col("cnt").desc()).limit(1).show()
