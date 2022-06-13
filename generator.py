bd70d6b05dc1e20c8ce07951e2712d558bd4efb3
Redhead <86776617+alin2025@users.noreply.github.com>
2022-06-13 23:52:00 +0300

    Update generator.py

--- generator.py
+++ generator.py
@@ -22,16 +22,16 @@ def random_string(stringLength=10):
     return ''.join(random.sample(letters, stringLength))
 
 
-print("Products between {} and {}".format(1, 250000))
-product_ids = [x for x in range(1, 250000)]
-dates = ['2022-07-01', '2022-07-02', '2022-07-03', '2022-07-04', '2022-07-05', '2022-07-06',
-         '2022-07-07', '2022-07-08', '2022-07-09', '2022-07-10']
+print("Products between {} and {}".format(1, 75000000))
+product_ids = [x for x in range(1, 75000000)]
+dates = ['2020-07-01', '2020-07-02', '2020-07-03', '2020-07-04', '2020-07-05', '2020-07-06', '2020-07-07', '2020-07-08',
+         '2020-07-09', '2020-07-10']
 seller_ids = [x for x in range(1, 10)]
 
 
 #   Generate products
 products = [[0, "product_0", 22]]
-for p in tqdm(product_ids,desc = 'products Progress Bar'):
+for p in tqdm(product_ids):
     products.append([p, "product_{}".format(p), random.randint(1, 150)])
 #   Save dataframe
 df = pd.DataFrame(products)
@@ -41,17 +41,17 @@ del df
 del products
 
 #   Generate sellers
-sellers = [[0, "seller_0", 250000]]
-for s in tqdm(seller_ids,desc = 'seller_ids Progress Bar'):
-    sellers.append([s, "seller_{}".format(s), random.randint(12000, 100000)])
+sellers = [[0, "seller_0", 2500000]]
+for s in tqdm(seller_ids):
+    sellers.append([s, "seller_{}".format(s), random.randint(12000, 2000000)])
 #   Save dataframe
 df = pd.DataFrame(sellers)
 df.columns = ["seller_id", "seller_name", "daily_target"]
 df.to_csv("sellers.csv", index=False)
 
 #   Generate sales
-total_rows = 50000
-prod_zero = int(total_rows * 0.25)
+total_rows = 500000
+prod_zero = int(total_rows * 0.95)
 prod_others = total_rows - prod_zero + 1
 df_array = [["order_id", "product_id", "seller_id", "date", "num_pieces_sold", "bill_raw_text"]]
 with open('sales.csv', 'w', newline='') as f:
@@ -59,7 +59,7 @@ with open('sales.csv', 'w', newline='') as f:
     csvwriter.writerows(df_array)
 
 order_id = 0
-for i in tqdm(range(0, 40),desc = 'sales Progress Bar'):
+for i in tqdm(range(0, 40)):
     df_array = []
 
     for i in range(0, prod_zero):
