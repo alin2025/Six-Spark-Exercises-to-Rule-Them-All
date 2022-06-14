e5610a9f73221d2108ca14bb2084a022b6430def
Redhead <86776617+alin2025@users.noreply.github.com>
2022-06-14 16:31:24 +0300

    Update warm_up_01.py

--- warm_up_01.py
+++ warm_up_01.py
@@ -13,13 +13,13 @@ spark = SparkSession.builder \
 #   Read the source tables in Parquet format
 products_table = spark.read.parquet("./products_parquet")
 sales_table = spark.read.parquet("./sales_parquet")
-sellers_table = spark.read.parquet("./sellers_parquet")
+# sellers_table = spark.read.parquet("./sellers_parquet")
 
 #   Print the number of orders
 print("Number of Orders: {}".format(sales_table.count()))
 
 #   Print the number of sellers
-print("Number of sellers: {}".format(sellers_table.count()))
+# print("Number of sellers: {}".format(sellers_table.count()))
 
 #   Print the number of products
 print("Number of products: {}".format(products_table.count()))
@@ -34,3 +34,4 @@ sales_table.groupBy(col("product_id")).agg(
     count("*").alias("cnt")).orderBy(col("cnt").desc()).limit(1).show()
 
 spark.stop()
+
